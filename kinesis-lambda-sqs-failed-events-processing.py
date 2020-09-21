4839fefc5880a0a0889ecea653b8786477cf07e7
Praveen Panati <25986697+prlabs@users.noreply.github.com>
2020-09-21 12:30:47 -0400

    Create kinesis-lambda-sqs-failed-events-processing.py
    this lambda function captures the kinesis batch metadata for the failed events sink and uses that metadata to retrieve the records from the kinesis stream
    
    more information at this link: https://docs.aws.amazon.com/lambda/latest/dg//with-kinesis.html#services-kinesis-errors

---
+++ kinesis-lambda-sqs-failed-events-processing.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import boto3
+import json
+
+def lambda_handler(event, context):
+    print ("sqs lambda function is being called...")
+    
+    #get the kinesis batch metadata
+    
+    for record in event['Records']:
+       payload=record["body"]
+       print("updated...15")
+       jsonData = json.loads(payload)
+       
+       print(jsonData)
+       
+    rshardId = jsonData['KinesisBatchInfo']['shardId']
+    rstreamArn = 'my-data-stream'
+    rstartSequenceNumber = jsonData['KinesisBatchInfo']['startSequenceNumber']
+    rendSequenceNumber = jsonData['KinesisBatchInfo']['endSequenceNumber']
+    rbatchSize = jsonData['KinesisBatchInfo']['batchSize']
+    
+    #use the meta data to get the iterator
+      
+    client = boto3.client('kinesis')
+
+    kresponse = client.get_shard_iterator(
+                    StreamName= rstreamArn,
+                    ShardId= rshardId,
+                    ShardIteratorType='AT_SEQUENCE_NUMBER',
+                    StartingSequenceNumber= rstartSequenceNumber
+            )
+    
+    shardIteratorJsonDataDump = json.dumps(kresponse)
+    
+    shardIteratorJsonData = json.loads(shardIteratorJsonDataDump)
+        
+    shardIteratorDataId = shardIteratorJsonData['ShardIterator']
+        
+    print('iteratorid: ' + shardIteratorDataId)
+    
+    #use the iterator to get the kinesis records
+    
+    kinesisErrorRecords = client.get_records(
+                    ShardIterator= shardIteratorDataId,
+                    Limit= rbatchSize
+    )
+    
+    print(str(kinesisErrorRecords))
+
+    
+
+
+    
